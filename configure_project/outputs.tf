151b1b786395be58a2000ab449f671fa746534aa
Rajat Agrawal <108140212+rajatagarwal-ibm@users.noreply.github.com>
2024-04-19 11:19:33 +0100

    Update outputs.tf

--- configure_project/outputs.tf
+++ configure_project/outputs.tf
@@ -2,3 +2,8 @@ output "project_id" {
   value       = trimprefix(resource.restapi_object.configure_project[0].id, "/v2/projects/")
   description = "ID of the created project"
 }
+
+output "project_id_object" {
+  value       = resource.restapi_object.configure_project[0]
+  description = "ID of the created project"
+}
