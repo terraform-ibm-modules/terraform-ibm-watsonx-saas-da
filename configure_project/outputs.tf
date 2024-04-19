20d1a19d11919a38ed911e2105b9d01256cea661
Rajat Agrawal <rajat.agrawal@ibm.com>
2024-04-19 14:01:50 +0100

    feat: project id regex

--- configure_project/outputs.tf
+++ configure_project/outputs.tf
@@ -1,9 +1,4 @@
 output "project_id" {
-  value       = trimprefix(resource.restapi_object.configure_project[0].id, "/v2/projects/")
-  description = "ID of the created project"
-}
-
-output "project_id_regex" {
   value       = local.project_id
   description = "Project ID Regex"
 }
