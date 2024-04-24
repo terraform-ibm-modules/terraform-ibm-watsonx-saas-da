08a3ac1bd275c57432b9722f9e6c8c15fe0d6108
hlucey <42588404+hlucey@users.noreply.github.com>
2024-04-24 16:44:23 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "ibmcloud_api_key" {
-  description = "Used with the Terraform IBM-Cloud/ibm provider."
+  description = "The API key that's used with the IBM Cloud Terraform and IBM provider."
   sensitive   = true
   type        = string
 }
 
 variable "watsonx_admin_api_key" {
   default     = null
-  description = "Used to call Watson APIs to configure the user and the project."
+  description = "The API key that's used to call Watson APIs to configure the user and the project."
   sensitive   = true
   type        = string
 }
 
 variable "location" {
   default     = "us-south"
-  description = "Used with the Terraform IBM-Cloud and IBM provider as well as during resource creation."
+  description = "The location that's used with the IBM Cloud Terraform and IBM provider as well as during resource creation."
   type        = string
   validation {
     condition     = contains(["eu-de", "us-south"], var.location)
@@ -29,37 +29,37 @@ variable "use_existing_resource_group" {
 
 variable "resource_group_name" {
   type        = string
-  description = "The name of a new or an existing resource group in which to provision resources to."
+  description = "The name of a new or an existing resource group where resources are created."
 }
 
 variable "resource_prefix" {
-  description = "The name to be used on all Watson resource as prefix."
+  description = "The name to be used on all Watson resources as a prefix."
   type        = string
   default     = "watsonx-poc"
 
   validation {
     condition     = var.resource_prefix != "" && length(var.resource_prefix) <= 25
-    error_message = "You must provide a value for the resource_prefix variable and the resource_prefix length must be less than 25 characters."
+    error_message = "You must provide a value for the resource_prefix variable and the resource_prefix length can't exceed 25 characters."
   }
 }
 
 variable "cos_plan" {
   default     = "standard"
-  description = "Resource plan used to provision the Cloud Object Storage instance."
+  description = "The resource plan that's used to provision the Cloud Object Storage instance."
   type        = string
   validation {
     condition     = contains(["standard"], var.cos_plan)
-    error_message = "The resource plan you use must be `standard`."
+    error_message = "You must use a standard plan. Standard plan instances are the most common and are recommended for most workloads."
   }
 }
 
 variable "watson_machine_learning_plan" {
   default     = "v2-standard"
-  description = "Resource plan used to provision the Watson Machine Learning instance."
+  description = "The resource plan that's used to provision the Watson Machine Learning instance. "
   type        = string
   validation {
     condition     = contains(["lite", "v2-professional", "v2-standard"], var.watson_machine_learning_plan)
-    error_message = "The resource plan you use must be `lite`, `v2-professional`, or `v2-standard`."
+    error_message = "The plan must be lite, v2-professional, or v2-standard. For more information about plans, see link."
   }
 }
 
@@ -69,13 +69,13 @@ variable "watson_studio_plan" {
   type        = string
   validation {
     condition     = contains(["free-v1", "professional-v1"], var.watson_studio_plan)
-    error_message = "The resource plan you use must be `free-v1` or `professional-v1`."
+    error_message = "You must use a free-v1 or professional-v1 plan. For more information about plans, see link."
   }
 }
 
 variable "watson_discovery_plan" {
   default     = "do not install"
-  description = "Resource plan used to provision the Watson Discovery instance."
+  description = "The resource plan that's used to provision the Watson Discovery instance."
   type        = string
   validation {
     condition = anytrue([
@@ -84,13 +84,13 @@ variable "watson_discovery_plan" {
       var.watson_discovery_plan == "enterprise",
       var.watson_discovery_plan == "premium",
     ])
-    error_message = "The resource plan you use must be `plus`, `enterprise`, or `premium`."
+    error_message = "You must use a plus, enterprise, or premium plan. For more information about the types of plans, see link."
   }
 }
 
 variable "watson_assistant_plan" {
   default     = "do not install"
-  description = "Resource plan used to provision the watsonx Assistance instance."
+  description = "The resource plan that's used to provision the watsonx Assistance instance."
   type        = string
   validation {
     condition = anytrue([
@@ -101,13 +101,13 @@ variable "watson_assistant_plan" {
       var.watson_assistant_plan == "enterprise",
       var.watson_assistant_plan == "enterprisedataisolation",
     ])
-    error_message = "The resource plan you use must be `free`, `trial`, `plus-trial`, `enterprise`, or `enterprisedataisolation`."
+    error_message = "You must use a free, trial, plus-trial, enterprise, or enterprisedataisolation plan. For more information about plans, see link."
   }
 }
 
 variable "watson_governance_plan" {
   default     = "do not install"
-  description = "Resource plan used to provision the watsonx Governance instance."
+  description = "Resource plan used to provision the watsonx Governance instance. The available plans depend on the region where you are provisioning the service from the IBM Cloud catalog."
   type        = string
   validation {
     condition = anytrue([
@@ -115,24 +115,24 @@ variable "watson_governance_plan" {
       var.watson_governance_plan == "lite",
       var.watson_governance_plan == "essentials",
     ])
-    error_message = "The resource plan you use must be `lite` or `essentials`."
+    error_message = "You must use a lite or essential plan. For more information about available plans, see https://dataplatform.cloud.ibm.com/docs/content/wsj/model/wos-plan-options.html?context=wx&audience=wdp."
   }
 }
 
 variable "watsonx_project_name" {
-  description = "Name of the watson project."
+  description = "The name of the watson project."
   type        = string
   default     = "demo"
 }
 
 variable "watsonx_project_description" {
-  description = "Description of the watson project."
+  description = "A description of the watson project that's created by the WatsonX.ai SaaS Deployable Architecture."
   type        = string
   default     = "Watson project created by the watsonx-ai SaaS deployable architecture."
 }
 
 variable "watsonx_project_tags" {
-  description = "Tags to attach to the watson project."
+  description = "A list of tags to attach to the watsonx project to help find it in watsonx."
   type        = list(string)
   default     = ["watsonx-ai-SaaS"]
 }
