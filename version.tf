0603c89662f955f0444116c5245eddb05580e746
ocofaigh <ocofaigh@ie.ibm.com>
2024-09-03 08:52:44 +0100

    fix: add workaround for auth policy backend replication delays

--- version.tf
+++ version.tf
@@ -1,13 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
+  # Lock DA into an exact provider version - renovate automation will keep it updated
   required_providers {
     restapi = {
       source  = "Mastercard/restapi"
-      version = ">= 1.19.1"
+      version = "1.20.0"
     }
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.66.0"
+      version = "1.68.1"
     }
   }
 }
