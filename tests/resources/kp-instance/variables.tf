f0819b07c0dc6733776abac35a5604d9319e1108
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2024-11-28 00:16:17 +0530

    add: existing kp

---
+++ tests/resources/kp-instance/variables.tf
@@ -0,0 +1,33 @@
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "The IBM Cloud API Key"
+  sensitive   = true
+}
+
+variable "region" {
+  type        = string
+  description = "Region to provision all resources created by this example"
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix to append to all resources created by this example"
+}
+
+variable "resource_group" {
+  type        = string
+  description = "An existing resource group name to use for this example. If unset a new resource group will be created"
+  default     = null
+}
+
+variable "resource_tags" {
+  type        = list(string)
+  description = "Optional list of tags to be added to created resources"
+  default     = []
+}
+
+variable "access_tags" {
+  type        = list(string)
+  description = "Optional list of access management tags to add to the Key Protect instance"
+  default     = []
+}
