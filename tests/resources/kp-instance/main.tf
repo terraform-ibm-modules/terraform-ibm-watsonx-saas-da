19cc7d58cdc50b84419f2f4e4bfd30e94ad5bdd8
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2024-11-28 00:26:02 +0530

    fix: pre-commit

--- tests/resources/kp-instance/main.tf
+++ tests/resources/kp-instance/main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
-  source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.1.6"
+  source                       = "terraform-ibm-modules/resource-group/ibm"
+  version                      = "1.1.6"
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
 }
