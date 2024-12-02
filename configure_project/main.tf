d60c09de7bc103f3374c9dc24509fe45e3238a98
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2024-12-02 10:59:37 +0530

    increase time for configure project

--- configure_project/main.tf
+++ configure_project/main.tf
@@ -60,7 +60,7 @@ resource "restapi_object" "configure_project" {
 
 resource "time_sleep" "wait_5_seconds" {
   depends_on      = [restapi_object.configure_project]
-  create_duration = "5s"
+  create_duration = "10s"
 }
 
 data "restapi_object" "get_project" {
