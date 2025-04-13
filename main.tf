7721be59fadc97223706698dde774e3f68915894
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-04-13 20:25:03 +0100

    fix(deps): update terraform-module (#258)

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ module "resource_group" {
     ibm = ibm.deployer
   }
   source                       = "terraform-ibm-modules/resource-group/ibm"
-  version                      = "1.1.6"
+  version                      = "1.2.0"
   resource_group_name          = var.use_existing_resource_group == false ? var.resource_group_name : null
   existing_resource_group_name = var.use_existing_resource_group == true ? var.resource_group_name : null
 }
@@ -85,7 +85,7 @@ module "cos" {
     ibm = ibm.deployer
   }
   source            = "terraform-ibm-modules/cos/ibm//modules/fscloud"
-  version           = "8.21.2"
+  version           = "8.21.8"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.resource_prefix}-cos-instance"
   cos_plan          = var.cos_plan
