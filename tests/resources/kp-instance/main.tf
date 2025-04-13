a1b929d361876ddb891a506f633415fe0ab27734
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-13 14:11:11 +0000

    fix(deps): update terraform-module

--- tests/resources/kp-instance/main.tf
+++ tests/resources/kp-instance/main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
-  version                      = "1.1.6"
+  version                      = "1.2.0"
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
 }
