012714ea5f7c52019a860c8bde6cc9475f14d291
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-10-11 02:08:38 +0530

    test: create existing cos instance

--- tests/resources/kp-instance/main.tf
+++ tests/resources/kp-instance/main.tf
@@ -1,9 +1,16 @@
+##############################################################################################################
+# RESOURCE GROUP
+##############################################################################################################
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
   version                      = "1.3.0"
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
 }
+
+##############################################################################################################
+# KMS
+##############################################################################################################
 
 module "key_protect_module" {
   source            = "terraform-ibm-modules/key-protect/ibm"
@@ -22,3 +29,19 @@ module "kms_root_key" {
   kms_instance_id = module.key_protect_module.key_protect_id
   key_name        = "test-r-key"
 }
+
+##############################################################################################################
+# Cloud Object Storage
+##############################################################################################################
+
+module "cos_module" {
+  source                     = "terraform-ibm-modules/cos/ibm"
+  version                    = "10.4.0"
+  resource_group_id          = module.resource_group.resource_group_id
+  region                     = var.region
+  cos_instance_name          = "${var.prefix}-cos"
+  existing_kms_instance_guid = module.key_protect_module.key_protect_guid
+  create_cos_bucket          = false
+}
+
+##############################################################################################################
--- tests/resources/kp-instance/outputs.tf
+++ tests/resources/kp-instance/outputs.tf
@@ -7,3 +7,8 @@ output "kms_key_crn" {
   value       = module.kms_root_key.crn
   description = "CRN of the KMS key"
 }
+
+output "cos_crn" {
+  value       = module.cos_module.cos_instance_crn
+  description = "CRN of Cloud Object Storage instance"
+}
