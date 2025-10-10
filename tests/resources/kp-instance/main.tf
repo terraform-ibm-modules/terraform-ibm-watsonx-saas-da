86c32b49230069f90796864838a8d2c55dbbce74
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-10-10 21:51:26 +0530

    chore: updated rg

--- tests/resources/kp-instance/main.tf
+++ tests/resources/kp-instance/main.tf
@@ -1,6 +1,7 @@
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
   version                      = "1.3.0"
+  resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
 }
 
