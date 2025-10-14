2a27996eca73f203f69064c4c6bd6e42fbe8cb97
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-10-14 08:12:51 +0530

    test: refactored tests and fixed issue in one of upgrade test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -80,6 +80,50 @@ func setupOptionsRootDA(t *testing.T, prefix string, dir string) *testhelper.Tes
 	return options
 }
 
+// Provision KMS - Key Protect to use in DA tests
+func setupKMSKeyProtect(t *testing.T, region string, prefix string) *terraform.Options {
+	realTerraformDir := "./resources/kp-instance"
+	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueId())))
+
+	checkVariable := "TF_VAR_ibmcloud_api_key"
+	val, present := os.LookupEnv(checkVariable)
+	require.True(t, present, checkVariable+" environment variable not set")
+	require.NotEqual(t, "", val, checkVariable+" environment variable is empty")
+
+	logger.Log(t, "Tempdir: ", tempTerraformDir)
+	existingTerraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: tempTerraformDir,
+		Vars: map[string]interface{}{
+			"prefix": prefix,
+			"region": region,
+		},
+		// Set Upgrade to true to ensure latest version of providers and modules are used by terratest.
+		// This is the same as setting the -upgrade=true flag with terraform.
+		Upgrade: true,
+	})
+
+	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
+	_, existErr := terraform.InitAndApplyE(t, existingTerraformOptions)
+	require.NoError(t, existErr, "Init and Apply of temp resources (KP Instance and Key creation) failed")
+
+	return existingTerraformOptions
+}
+
+// Cleanup the resources when KMS encryption key is created.
+func cleanupResources(t *testing.T, terraformOptions *terraform.Options, prefix string) {
+	// Check if "DO_NOT_DESTROY_ON_FAILURE" is set
+	envVal, _ := os.LookupEnv("DO_NOT_DESTROY_ON_FAILURE")
+	// Destroy the temporary existing resources if required
+	if t.Failed() && strings.ToLower(envVal) == "true" {
+		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
+	} else {
+		logger.Log(t, "START: Destroy (existing resources)")
+		terraform.Destroy(t, terraformOptions)
+		terraform.WorkspaceDelete(t, terraformOptions, prefix)
+		logger.Log(t, "END: Destroy (existing resources)")
+	}
+}
+
 func TestRunRootDA(t *testing.T) {
 	t.Parallel()
 
@@ -105,173 +149,91 @@ func TestRunUpgradeRootDA(t *testing.T) {
 func TestWithExistingKP(t *testing.T) {
 	t.Parallel()
 
-	// ------------------------------------------------------------------------------------
-	// Provision KP first
-	// ------------------------------------------------------------------------------------
-
-	prefix := fmt.Sprintf("kp-wx-%s", strings.ToLower(random.UniqueId()))
-	realTerraformDir := "./resources/kp-instance"
-	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueId())))
 	region := validRegions[rand.Intn(len(validRegions))]
+	prefix := fmt.Sprintf("kp-wx-%s", strings.ToLower(random.UniqueId()))
 
-	// Verify ibmcloud_api_key variable is set
-	checkVariable := "TF_VAR_ibmcloud_api_key"
-	val, present := os.LookupEnv(checkVariable)
-	require.True(t, present, checkVariable+" environment variable not set")
-	require.NotEqual(t, "", val, checkVariable+" environment variable is empty")
+	// Provision Existing KMS instance
+	existingTerraformOptions := setupKMSKeyProtect(t, region, prefix)
 
-	logger.Log(t, "Tempdir: ", tempTerraformDir)
-	existingTerraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: tempTerraformDir,
-		Vars: map[string]interface{}{
-			"prefix": prefix,
-			"region": region,
+	// Deploy watsonx DA using existing KP details
+	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
+		Testing:      t,
+		TerraformDir: rootDaDir,
+		IgnoreDestroys: testhelper.Exemptions{ // Ignore for consistency check
+			List: []string{
+				"module.configure_user.null_resource.configure_user",
+				"module.configure_user.null_resource.restrict_access",
+			},
 		},
-		// Set Upgrade to true to ensure latest version of providers and modules are used by terratest.
-		// This is the same as setting the -upgrade=true flag with terraform.
-		Upgrade: true,
+		IgnoreUpdates: testhelper.Exemptions{ // Ignore for consistency check
+			List: []string{
+				"module.configure_user.null_resource.configure_user",
+				"module.configure_user.null_resource.restrict_access",
+			},
+		},
 	})
 
-	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
-	_, existErr := terraform.InitAndApplyE(t, existingTerraformOptions)
-	if existErr != nil {
-		assert.True(t, existErr == nil, "Init and Apply of temp existing resource failed")
-	} else {
-
-		// ------------------------------------------------------------------------------------
-		// Deploy watsonx DA passing in existing KP details
-		// ------------------------------------------------------------------------------------
-
-		options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
-			Testing:      t,
-			TerraformDir: rootDaDir,
-			IgnoreDestroys: testhelper.Exemptions{ // Ignore for consistency check
-				List: []string{
-					"module.configure_user.null_resource.configure_user",
-					"module.configure_user.null_resource.restrict_access",
-				},
-			},
-			IgnoreUpdates: testhelper.Exemptions{ // Ignore for consistency check
-				List: []string{
-					"module.configure_user.null_resource.configure_user",
-					"module.configure_user.null_resource.restrict_access",
-				},
-			},
-			TerraformVars: map[string]interface{}{
-				"location":                  region,
-				"resource_group_name":       prefix,
-				"provider_visibility":       "public",
-				"enable_cos_kms_encryption": true,
-				"cos_kms_crn":               terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
-				"cos_kms_key_crn":           terraform.Output(t, existingTerraformOptions, "kms_key_crn"),
-				"existing_cos_instance_crn": terraform.Output(t, existingTerraformOptions, "cos_crn"),
-				"resource_prefix":           prefix,
-			},
-		})
-
-		output, err := options.RunTestConsistency()
-		assert.Nil(t, err, "This should not have errored")
-		assert.NotNil(t, output, "Expected some output")
+	options.TerraformVars = map[string]interface{}{
+		"location":                  region,
+		"resource_group_name":       prefix,
+		"provider_visibility":       "public",
+		"enable_cos_kms_encryption": true,
+		"cos_kms_crn":               terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
+		"cos_kms_key_crn":           terraform.Output(t, existingTerraformOptions, "kms_key_crn"),
+		"existing_cos_instance_crn": terraform.Output(t, existingTerraformOptions, "cos_crn"),
+		"resource_prefix":           prefix,
 	}
 
-	// Check if "DO_NOT_DESTROY_ON_FAILURE" is set
-	envVal, _ := os.LookupEnv("DO_NOT_DESTROY_ON_FAILURE")
-	// Destroy the temporary existing resources if required
-	if t.Failed() && strings.ToLower(envVal) == "true" {
-		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
-	} else {
-		logger.Log(t, "START: Destroy (existing resources)")
-		terraform.Destroy(t, existingTerraformOptions)
-		terraform.WorkspaceDelete(t, existingTerraformOptions, prefix)
-		logger.Log(t, "END: Destroy (existing resources)")
-	}
+	output, err := options.RunTestConsistency()
+	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
 
+	cleanupResources(t, existingTerraformOptions, prefix)
 }
 
 func TestRunUpgradeExistingKP(t *testing.T) {
 	t.Parallel()
 
-	// ------------------------------------------------------------------------------------
-	// Provision KP first
-	// ------------------------------------------------------------------------------------
-
-	prefix := fmt.Sprintf("kp-ut-%s", strings.ToLower(random.UniqueId()))
-	realTerraformDir := "./resources/kp-instance"
-	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueId())))
 	region := validRegions[rand.Intn(len(validRegions))]
+	prefix := fmt.Sprintf("kp-ut-%s", strings.ToLower(random.UniqueId()))
 
-	// Verify ibmcloud_api_key variable is set
-	checkVariable := "TF_VAR_ibmcloud_api_key"
-	val, present := os.LookupEnv(checkVariable)
-	require.True(t, present, checkVariable+" environment variable not set")
-	require.NotEqual(t, "", val, checkVariable+" environment variable is empty")
+	// Provision Existing KMS instance
+	existingTerraformOptions := setupKMSKeyProtect(t, region, prefix)
 
-	logger.Log(t, "Tempdir: ", tempTerraformDir)
-	existingTerraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: tempTerraformDir,
-		Vars: map[string]interface{}{
-			"prefix": prefix,
-			"region": region,
+	// Deploy watsonx DA using existing KP details
+	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
+		Testing:      t,
+		TerraformDir: rootDaDir,
+		Prefix:       "existing-kp-upg",
+		IgnoreDestroys: testhelper.Exemptions{ // Ignore for consistency check
+			List: []string{
+				"module.configure_user.null_resource.configure_user",
+				"module.configure_user.null_resource.restrict_access",
+			},
 		},
-		// Set Upgrade to true to ensure latest version of providers and modules are used by terratest.
-		// This is the same as setting the -upgrade=true flag with terraform.
-		Upgrade: true,
+		IgnoreUpdates: testhelper.Exemptions{ // Ignore for consistency check
+			List: []string{
+				"module.configure_user.null_resource.configure_user",
+				"module.configure_user.null_resource.restrict_access",
+			},
+		},
 	})
-
-	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
-	_, existErr := terraform.InitAndApplyE(t, existingTerraformOptions)
-	if existErr != nil {
-		assert.True(t, existErr == nil, "Init and Apply of temp existing resource failed")
-	} else {
-
-		// ------------------------------------------------------------------------------------
-		// Upgrade test for watsonx DA passing in existing KP details
-		// ------------------------------------------------------------------------------------
-
-		options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
-			Testing:      t,
-			TerraformDir: rootDaDir,
-			Prefix:       "existing-kp-upg",
-			IgnoreDestroys: testhelper.Exemptions{ // Ignore for consistency check
-				List: []string{
-					"module.configure_user.null_resource.configure_user",
-					"module.configure_user.null_resource.restrict_access",
-				},
-			},
-			IgnoreUpdates: testhelper.Exemptions{ // Ignore for consistency check
-				List: []string{
-					"module.configure_user.null_resource.configure_user",
-					"module.configure_user.null_resource.restrict_access",
-				},
-			},
-			TerraformVars: map[string]interface{}{
-				"location":                  region,
-				"resource_group_name":       prefix,
-				"provider_visibility":       "public",
-				"enable_cos_kms_encryption": true,
-				"cos_kms_crn":               terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
-				"existing_cos_instance_crn": terraform.Output(t, existingTerraformOptions, "cos_crn"),
-				"resource_prefix":           prefix,
-			},
-		})
-
-		output, err := options.RunTestUpgrade()
-		if !options.UpgradeTestSkipped {
-			assert.Nil(t, err, "This should not have errored")
-			assert.NotNil(t, output, "Expected some output")
-		}
+	options.TerraformVars = map[string]interface{}{
+		"location":                  region,
+		"resource_group_name":       prefix,
+		"provider_visibility":       "public",
+		"enable_cos_kms_encryption": true,
+		"cos_kms_crn":               terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
+		"cos_kms_key_crn":           terraform.Output(t, existingTerraformOptions, "kms_key_crn"),
+		"existing_cos_instance_crn": terraform.Output(t, existingTerraformOptions, "cos_crn"),
+		"resource_prefix":           prefix,
 	}
 
-	// Check if "DO_NOT_DESTROY_ON_FAILURE" is set
-	envVal, _ := os.LookupEnv("DO_NOT_DESTROY_ON_FAILURE")
-	// Destroy the temporary existing resources if required
-	if t.Failed() && strings.ToLower(envVal) == "true" {
-		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
-	} else {
-		logger.Log(t, "START: Destroy (existing resources)")
-		terraform.Destroy(t, existingTerraformOptions)
-		terraform.WorkspaceDelete(t, existingTerraformOptions, prefix)
-		logger.Log(t, "END: Destroy (existing resources)")
+	output, err := options.RunTestUpgrade()
+	if !options.UpgradeTestSkipped {
+		assert.Nil(t, err, "This should not have errored")
+		assert.NotNil(t, output, "Expected some output")
 	}
 
+	cleanupResources(t, existingTerraformOptions, prefix)
 }
