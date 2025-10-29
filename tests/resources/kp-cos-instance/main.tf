9b4baa58472a3eb610bb09478145ef3dcdcd4538
Ayushmaan Singh <ayushmaan017+github@gmail.com>
2025-10-29 18:04:45 +0530

    fix: resource group fixed in test main.tf

--- tests/resources/kp-cos-instance/main.tf
+++ tests/resources/kp-cos-instance/main.tf
@@ -4,7 +4,7 @@
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
   version                      = "1.4.0"
-  existing_resource_group_name = var.existing_resource_group_name
+  existing_resource_group_name = var.existing_resource_group_name == null ? "${var.prefix}-resource-group" : null
 }
 
 ##############################################################################################################
