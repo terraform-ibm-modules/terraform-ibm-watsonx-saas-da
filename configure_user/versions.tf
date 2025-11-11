09a5834fe32f4c640ffe2244f2848506439edf12
ocofaigh <ocofaigh@ie.ibm.com>
2025-11-11 22:19:45 +0000

    fix bug

--- configure_user/versions.tf
+++ configure_user/versions.tf
@@ -2,8 +2,9 @@ terraform {
   required_version = ">= 1.9.0"
   required_providers {
     ibm = {
-      source  = "IBM-Cloud/ibm"
-      version = ">= 1.80.0, <2.0.0"
+      source                = "IBM-Cloud/ibm"
+      version               = ">= 1.80.0, <2.0.0"
+      configuration_aliases = [ibm.deployer]
     }
     null = {
       source  = "hashicorp/null"
