c8fd22e7299854d9d38f2dcd0432b4649be3f6ab
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-12-14 14:56:26 +0000

    fix(deps): update terraform terraform-ibm-modules/key-protect/ibm to v2.10.34

--- tests/resources/kp-cos-instance/main.tf
+++ tests/resources/kp-cos-instance/main.tf
@@ -14,7 +14,7 @@ module "resource_group" {
 
 module "key_protect_module" {
   source            = "terraform-ibm-modules/key-protect/ibm"
-  version           = "2.10.32"
+  version           = "2.10.34"
   key_protect_name  = "${var.prefix}-kp"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
