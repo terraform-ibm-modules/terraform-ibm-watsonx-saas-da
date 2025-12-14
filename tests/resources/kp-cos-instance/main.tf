b6b18bf562a49ac3223e0178249cf603337664ae
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-14 08:18:01 +0000

    fix(deps): update dependencies (#356)

--- tests/resources/kp-cos-instance/main.tf
+++ tests/resources/kp-cos-instance/main.tf
@@ -14,7 +14,7 @@ module "resource_group" {
 
 module "key_protect_module" {
   source            = "terraform-ibm-modules/key-protect/ibm"
-  version           = "2.10.30"
+  version           = "2.10.32"
   key_protect_name  = "${var.prefix}-kp"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
@@ -25,7 +25,7 @@ module "key_protect_module" {
 
 module "kms_root_key" {
   source          = "terraform-ibm-modules/kms-key/ibm"
-  version         = "1.4.9"
+  version         = "1.4.11"
   kms_instance_id = module.key_protect_module.key_protect_id
   key_name        = "test-r-key"
 }
