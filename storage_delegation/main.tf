b2e8ff8c4ab8f1ba2f6b408965aa6d3e2f328b23
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-21 04:21:16 +0000

    fix(deps): update dependencies (#361)

--- storage_delegation/main.tf
+++ storage_delegation/main.tf
@@ -1,6 +1,6 @@
 module "kms_key_crn_parser" {
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.3.6"
+  version = "1.3.7"
   crn     = data.ibm_kms_key.kms_key.keys[0].crn
 }
 
