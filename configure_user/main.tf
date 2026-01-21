a83e933989f14d33514984a7331888ee2cff5bb6
Arya Girish K <arya.girish.k@ibm.com>
2026-01-21 09:47:12 +0530

    feat: add binary installation support for Waypoint runtime compatibility

--- configure_user/main.tf
+++ configure_user/main.tf
@@ -2,16 +2,31 @@ data "ibm_iam_auth_token" "tokendata" {}
 
 locals {
   sensitive_tokendata = sensitive(data.ibm_iam_auth_token.tokendata.iam_access_token)
+  binaries_path = "/tmp"
 }
 
-resource "null_resource" "configure_user" {
-  depends_on = [data.ibm_iam_auth_token.tokendata]
-  triggers = {
+resource "terraform_data" "install_required_binaries" {
+  count = var.install_required_binaries ? 1 : 0
+  triggers_replace    = {
+    iam_token         = local.sensitive_tokendata
+    resource_group_id = var.resource_group_id
+    location          = var.region
+    
+  }
+  provisioner "local-exec" {
+    command     = "${path.module}/scripts/install-binaries.sh ${local.binaries_path}"
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "terraform_data" "configure_user" {
+  depends_on = [data.ibm_iam_auth_token.tokendata,terraform_data.install_required_binaries]
+  triggers_replace = {
     always_run = timestamp()
   }
 
   provisioner "local-exec" {
-    command     = "${path.module}/scripts/add_user.sh"
+    command     = "${path.module}/scripts/add_user.sh ${local.binaries_path}"
     interpreter = ["/bin/bash", "-c"]
     environment = {
       iam_token         = local.sensitive_tokendata
@@ -21,14 +36,14 @@ resource "null_resource" "configure_user" {
   }
 }
 
-resource "null_resource" "restrict_access" {
-  depends_on = [data.ibm_iam_auth_token.tokendata]
-  triggers = {
+resource "terraform_data" "restrict_access" {
+  depends_on = [data.ibm_iam_auth_token.tokendata,terraform_data.install_required_binaries]
+  triggers_replace = {
     always_run = timestamp()
   }
 
   provisioner "local-exec" {
-    command     = "${path.module}/scripts/enforce_account_restriction.sh"
+    command     = "${path.module}/scripts/enforce_account_restriction.sh ${local.binaries_path}"
     interpreter = ["/bin/bash", "-c"]
     environment = {
       iam_token = local.sensitive_tokendata
--- configure_user/scripts/enforce_account_restriction.sh
+++ configure_user/scripts/enforce_account_restriction.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# The binaries downloaded by the install-binaries script are located in the /tmp directory.
+export PATH=$PATH:${1:-"/tmp"}
+
 # shellcheck disable=SC2154
 token="$(echo "$iam_token" | awk '{print $2}')"
 
