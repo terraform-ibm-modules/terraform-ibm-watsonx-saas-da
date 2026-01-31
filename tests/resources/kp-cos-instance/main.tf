0a033e5b7a4a2d28c121a347046bc96f1b9ee091
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-31 09:09:49 +0000

    fix(deps): update dependencies

--- tests/resources/kp-cos-instance/main.tf
+++ tests/resources/kp-cos-instance/main.tf
@@ -36,7 +36,7 @@ module "kms_root_key" {
 
 module "cos_module" {
   source                     = "terraform-ibm-modules/cos/ibm"
-  version                    = "10.9.7"
+  version                    = "10.9.8"
   resource_group_id          = module.resource_group.resource_group_id
   region                     = var.region
   cos_instance_name          = "${var.prefix}-cos"
