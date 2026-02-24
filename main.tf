bae7547f5d2cdad29beeca21ee3e861b64ec6331
zikra-iqbal <zikra.iqbalmnnit@gmail.com>
2026-02-24 14:39:24 +0530

    updated COS version

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ module "cos" {
   }
   count             = var.existing_cos_instance_crn == null ? 1 : 0
   source            = "terraform-ibm-modules/cos/ibm//modules/fscloud"
-  version           = "10.14.1"
+  version           = "10.14.2"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${local.prefix}cos-instance"
   cos_plan          = var.cos_plan
