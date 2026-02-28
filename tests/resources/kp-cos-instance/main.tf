dd3fb1bd6a816f4a4ff681311f9dbb116cb2af8b
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-02-28 16:16:41 +0000

    fix(deps): pin dependencies

--- tests/resources/kp-cos-instance/main.tf
+++ tests/resources/kp-cos-instance/main.tf
@@ -14,7 +14,7 @@ module "resource_group" {
 
 module "key_protect_module" {
   source            = "terraform-ibm-modules/key-protect/ibm"
-  version           = "2.10.57"
+  version           = "2.10.59"
   key_protect_name  = "${var.prefix}-kp"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
@@ -36,7 +36,7 @@ module "kms_root_key" {
 
 module "cos_module" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "10.14.2"
+  version           = "10.14.5"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   cos_instance_name = "${var.prefix}-cos"
