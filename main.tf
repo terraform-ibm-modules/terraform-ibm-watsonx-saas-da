9444c884ccf2be9bbc4a82ab4be8c08147b4bb10
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-04-19 04:55:08 +0000

    fix(deps): update dependencies

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "resource_group" {
     ibm = ibm.deployer
   }
   source                       = "terraform-ibm-modules/resource-group/ibm"
-  version                      = "1.5.0"
+  version                      = "1.6.0"
   existing_resource_group_name = var.existing_resource_group_name
 }
 
@@ -85,21 +85,21 @@ locals {
 module "existing_cos_crn_parser" {
   count   = var.existing_cos_instance_crn != null ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.4.2"
+  version = "1.5.0"
   crn     = var.existing_cos_instance_crn
 }
 
 module "cos_kms_key_crn_parser" {
   count   = var.enable_cos_kms_encryption && var.cos_kms_key_crn != null ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.4.2"
+  version = "1.5.0"
   crn     = var.cos_kms_key_crn
 }
 
 module "cos_kms_crn_parser" {
   count   = var.enable_cos_kms_encryption && var.cos_kms_key_crn == null ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.4.2"
+  version = "1.5.0"
   crn     = var.cos_kms_crn
 }
 
@@ -165,7 +165,7 @@ module "cos" {
   }
   count             = var.existing_cos_instance_crn == null ? 1 : 0
   source            = "terraform-ibm-modules/cos/ibm//modules/fscloud"
-  version           = "10.14.9"
+  version           = "10.15.3"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${local.prefix}cos-instance"
   cos_plan          = var.cos_plan
@@ -409,7 +409,7 @@ module "configure_user" {
     ibm = ibm.deployer
   }
   source            = "terraform-ibm-modules/watsonx-ai/ibm//modules/configure_user"
-  version           = "2.16.2"
+  version           = "2.17.0"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
 }
@@ -420,7 +420,7 @@ module "configure_user" {
 
 module "storage_delegation" {
   source  = "terraform-ibm-modules/watsonx-ai/ibm//modules/storage_delegation"
-  version = "2.16.2"
+  version = "2.17.0"
   count   = var.enable_cos_kms_encryption ? 1 : 0
   providers = {
     ibm     = ibm.deployer
@@ -437,7 +437,7 @@ module "storage_delegation" {
 
 module "configure_project" {
   source  = "terraform-ibm-modules/watsonx-ai/ibm//modules/configure_project"
-  version = "2.16.2"
+  version = "2.17.0"
   providers = {
     restapi = restapi.restapi_watsonx_admin
   }
