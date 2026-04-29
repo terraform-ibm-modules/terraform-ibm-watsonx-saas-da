051d017ec21846baa53f4ce82c2b081e30d5c300
zikra-iqbal <zikra.iqbalmnnit@gmail.com>
2026-04-29 09:50:38 +0530

    fix: revert restapi version

--- version.tf
+++ version.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     restapi = {
       source  = "Mastercard/restapi"
-      version = "3.0.0"
+      version = "2.0.1"
     }
     ibm = {
       source  = "IBM-Cloud/ibm"
