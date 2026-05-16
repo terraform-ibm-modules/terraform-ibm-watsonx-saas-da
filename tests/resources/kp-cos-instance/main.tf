6792d87d1abd680f575f485c0fe11fa139d221b1
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-05-16 10:00:16 +0000

    chore(deps): update dependencies

--- tests/resources/kp-cos-instance/main.tf
+++ tests/resources/kp-cos-instance/main.tf
@@ -14,7 +14,7 @@ module "resource_group" {
 
 module "key_protect_module" {
   source            = "terraform-ibm-modules/key-protect/ibm"
-  version           = "2.11.2"
+  version           = "2.13.0"
   key_protect_name  = "${var.prefix}-kp"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
